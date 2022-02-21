9b87f628065b235df4317763eb6978afb2744706
beherit197777 <beherit197777@gmail.com>
2022-02-21 13:51:35 +0200

    add lightgallery-stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "lightgallery.js/dist/css/lightgallery.min.css"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
